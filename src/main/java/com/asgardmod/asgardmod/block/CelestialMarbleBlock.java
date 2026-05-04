0c75dcd001cda826e9630c37c3fd21e1b94c4dbf
Rexifay <67446346+RexiFay@users.noreply.github.com>
2026-05-03 21:08:35 -0400

    remove: right-click toggle from CelestialMarbleBlock

--- src/main/java/com/asgardmod/asgardmod/block/CelestialMarbleBlock.java
+++ src/main/java/com/asgardmod/asgardmod/block/CelestialMarbleBlock.java
@@ -1,16 +1,11 @@
 package com.asgardmod.asgardmod.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.phys.BlockHitResult;
 
 public class CelestialMarbleBlock extends Block {
 
@@ -26,17 +21,7 @@ public class CelestialMarbleBlock extends Block {
         builder.add(ACTIVATED);
     }
 
-    @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos,
-                                 Player player, InteractionHand hand, BlockHitResult hit) {
-        if (!level.isClientSide) {
-            boolean current = state.getValue(ACTIVATED);
-            level.setBlock(pos, state.setValue(ACTIVATED, !current), 3);
-        }
-        return InteractionResult.sidedSuccess(level.isClientSide);
-    }
-
-    // Light up when activated
+    // Light up when activated by the portal
     @SuppressWarnings("deprecation")
     @Override
     public int getLightEmission(BlockState state, net.minecraft.world.level.BlockGetter level, BlockPos pos) {
